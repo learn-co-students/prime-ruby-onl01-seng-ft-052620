d97561579fbac3edd39971ba454b5c2a75780c6a
Daniel Andrew <danwandrew@gmail.com>
2020-05-27 12:12:44 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
 # Add  code here!
+def prime?(num)
+  if 1 >= num ;
+     return false
+  end
+  i=1;
+  while i<num
+    if i !=1 && i!=num && num%i ==0
+     return false;
+    end
+    i+=1;
+  end
+  true;
+end
