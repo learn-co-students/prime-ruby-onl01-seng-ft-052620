17a9701e7b6fe1a008f74582a6a1a1889bbb44b4
sean-slaughter <sthompson2822@gmail.com>
2020-05-28 11:53:05 -0500

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
 # Add  code here!
+def prime?(num)
+    if num == 0 
+        return false
+    elsif num < 0
+        num = -num
+    end
+    if num == 1
+        return false
+    end
+    (2..(num - 1)).each do |n|
+        return false if num % n == 0
+    end
+    true
+end
