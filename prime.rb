8142c3a168b45d4ede7845fc7fc63750cab6ae37
Maloochow <luyao.mly@gmail.com>
2020-05-29 18:37:25 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,21 @@
-# Add  code here!
+
+# def prime?(number)
+#     if number <= 1
+#         false
+#     elsif number == 2
+#         true
+#     else
+#         range = (2..number-1).to_a
+#         !range.any?{|x| number % x == 0}
+#     end
+# end
+
+
+
+def prime?(number)
+    if number <= 1
+        false
+    else
+    !(2..number-1).any?{|x| number % x == 0}
+    end
+end
