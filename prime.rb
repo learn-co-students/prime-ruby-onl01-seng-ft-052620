9b3b8fab8091611d5dd5da6c7f26c23502083a25
seanbalke <sean.balke@gmail.com>
2020-05-30 13:00:06 -0500

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,18 @@
-# Add  code here!
+require 'pry'
+# RTN TRUE FOR PRIME NUMBERS
+# RTN FALSE FOR NON-PRIME NUMBERS
+
+def prime?(num)
+    # binding.pry
+    return false if num < 2
+    n = 2
+    while n < num
+        return false if num % n == 0
+        n += 1
+    end
+    true
+end
+
+# is_prime?(4)
+
+# 2,3,4,5
