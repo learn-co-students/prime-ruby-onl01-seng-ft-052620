c76fe2fdc6a6611c77e528d27b831d398c7cbbd9
Kristin Howard <hkristin@andrew.cmu.edu>
2020-05-31 20:29:44 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
-# Add  code here!
+def prime?(num)
+
+if num <= 1
+  false
+else 
+  
+counter = 2
+  while counter < num 
+   if num % counter == 0
+    return false
+   end
+    counter += 1
+   end
+true
+end
+end
+  
