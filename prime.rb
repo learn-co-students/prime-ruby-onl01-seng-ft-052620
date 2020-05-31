926f41b1e3ca056ff2762d09fab3f7195453a605
Nroulston <nicolas.roulston@gmail.com>
2020-05-31 08:19:16 -0700

    Done.

--- prime.rb
+++ prime.rb
@@ -1,4 +1,16 @@
+
+
 require 'prime'
 def prime?(num)
-  Prime.prime?(num)
+  n = 2
+  return false if num <= 1 
+  while n < num
+     if num % n != 0
+       n += 1
+       next
+     else 
+      return false 
+     end
+  end
+  true
 end
