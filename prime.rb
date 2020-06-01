b18d6b11614e8eec87bcf82e60e6a00b84321472
Eriberto <eriberto.guzman@outlook.com>
2020-06-01 15:12:55 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
 # Add  code here!
+
+def prime?(num)
+    if num <= 1
+        return false
+    end
+       
+    (2..(num-1)).each do |prime_checker|
+        return false if num % prime_checker == 0
+    end
+    true
+
+end
