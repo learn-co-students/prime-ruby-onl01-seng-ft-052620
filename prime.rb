6ac67846f50c04293bf5fd3ce09eb7ccae95793d
Jonathan Bleibdrey <jonbleibdrey@aol.com>
2020-06-01 00:52:52 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,18 @@
-# Add  code here!
+#prime=[2,3,5,7]
+#composite=[1,4,6]
+
+def prime?(num)
+  if num == 0
+    return false
+  elsif num < 0
+    num = -num
+  end
+  if num == 1
+    return false
+  end
+(2..(num - 1)).each do |n|
+    return false if num % n == 0
+  end
+  true
+end
+
